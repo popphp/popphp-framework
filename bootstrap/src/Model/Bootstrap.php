43b6d8b21ec8202a56aa419a79d029f3f7abfd36
Nick Sagona <nick@nolainteractive.com>
2018-06-21 17:11:22 -0500

    Work on bootstrap feature

--- bootstrap/src/Model/Bootstrap.php
+++ bootstrap/src/Model/Bootstrap.php
@@ -45,6 +45,8 @@ class Bootstrap extends \Pop\Model\AbstractModel
         mkdir($location . '/public');
 
         $this->createHttpConfig($location, $namespace);
+        $this->createHttpController($location, $namespace);
+        $this->createViews($location);
     }
 
     /**
@@ -69,6 +71,9 @@ class Bootstrap extends \Pop\Model\AbstractModel
 
         $this->createHttpConfig($location, $namespace . '\\Http');
         $this->createConsoleConfig($location, $namespace);
+        $this->createHttpController($location, $namespace);
+        $this->createConsoleController($location, $namespace);
+        $this->createViews($location);
     }
 
     /**
@@ -123,5 +128,100 @@ class Bootstrap extends \Pop\Model\AbstractModel
         $consoleConfig->appendToBody('];');
 
         $consoleConfig->save();
+    }
+
+    /**
+     * Create HTTP controller method
+     *
+     * @param  string $location
+     * @param  string $namespace
+     * @return void
+     */
+    protected function createHttpController($location, $namespace)
+    {
+        $index = new Code\Generator($location . '/public/index.php', Code\Generator::CREATE_EMPTY);
+
+        $index->appendToBody("\$autoloader = include __DIR__ . '/../vendor/autoload.php';");
+        $index->appendToBody("");
+        $index->appendToBody("try {");
+        $index->appendToBody("    \$app = new Pop\\Application(\$autoloader, include __DIR__ . '/../app/config/app.http.php');");
+        $index->appendToBody("    \$app->register(new " .  $namespace . "\\Module());");
+        $index->appendToBody("    \$app->run();");
+        $index->appendToBody("} catch (\\Exception \$exception) {");
+        $index->appendToBody("    \$app = new " .  $namespace . "\\Module();");
+        $index->appendToBody("    \$app->httpError(new Pop\\Application(include __DIR__ . '/../app/config/app.http.php'), \$exception);");
+        $index->appendToBody("}");
+
+        $index->save();
+
+        $htaccess  = "<IfModule mod_rewrite.c>" . PHP_EOL;
+        $htaccess .= "    RewriteEngine On" . PHP_EOL . PHP_EOL;
+        $htaccess .= "    RewriteCond %{REQUEST_FILENAME} -s [OR]" . PHP_EOL;
+        $htaccess .= "    RewriteCond %{REQUEST_FILENAME} -l [OR]" . PHP_EOL;
+        $htaccess .= "    RewriteCond %{REQUEST_FILENAME} -f [OR]" . PHP_EOL;
+        $htaccess .= "    RewriteCond %{REQUEST_FILENAME} -d" . PHP_EOL . PHP_EOL;
+        $htaccess .= "    RewriteRule ^.*$ - [NC,L]" . PHP_EOL;
+        $htaccess .= "    RewriteRule ^.*$ index.php [NC,L]" . PHP_EOL;
+        $htaccess .= "</IfModule>" . PHP_EOL;
+
+        file_put_contents($location . '/public/.htaccess', $htaccess);
+    }
+
+    /**
+     * Create console controller method
+     *
+     * @param  string $location
+     * @param  string $namespace
+     * @return void
+     */
+    protected function createConsoleController($location, $namespace)
+    {
+        $app = new Code\Generator($location . '/script/app', Code\Generator::CREATE_EMPTY);
+
+        $app->appendToBody("\$autoloader = include __DIR__ . '/../vendor/autoload.php';");
+        $app->appendToBody("");
+        $app->appendToBody("try {");
+        $app->appendToBody("    \$app = new Pop\\Application(\$autoloader, include __DIR__ . '/../app/config/app.console.php');");
+        $app->appendToBody("    \$app->register(new " .  $namespace . "\\Module());");
+        $app->appendToBody("    \$app->run();");
+        $app->appendToBody("} catch (\\Exception \$exception) {");
+        $app->appendToBody("    \$app = new " .  $namespace . "\\Module();");
+        $app->appendToBody("    \$app->cliError(\$exception);");
+        $app->appendToBody("}");
+
+        $app->save();
+    }
+
+    /**
+     * Create views method
+     *
+     * @param  string $location
+     * @return void
+     */
+    protected function createViews($location)
+    {
+        $index  = "<!DOCTYPE html>" . PHP_EOL;
+        $index .= "<html>" . PHP_EOL;
+        $index .= "<head>" . PHP_EOL;
+        $index .= "    <title><?=\$title; ?></title>" . PHP_EOL;
+        $index .= "</head>" . PHP_EOL;
+        $index .= "<body>" . PHP_EOL;
+        $index .= "    <h1><?=\$title; ?></h1>" . PHP_EOL;
+        $index .= "</body>" . PHP_EOL;
+        $index .= "</html>" . PHP_EOL;
+
+        file_put_contents($location . '/view/index.phtml', $index);
+
+        $error  = "<!DOCTYPE html>" . PHP_EOL;
+        $error .= "<html>" . PHP_EOL;
+        $error .= "<head>" . PHP_EOL;
+        $error .= "    <title><?=\$title; ?></title>" . PHP_EOL;
+        $error .= "</head>" . PHP_EOL;
+        $error .= "<body>" . PHP_EOL;
+        $error .= "    <h1 style=\"color: #f00;\"><?=\$title; ?></h1>" . PHP_EOL;
+        $error .= "</body>" . PHP_EOL;
+        $error .= "</html>" . PHP_EOL;
+
+        file_put_contents($location . '/view/error.phtml', $error);
     }
 }
