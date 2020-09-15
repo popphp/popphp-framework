97b67e88baa87a602edfae627741e7e23c6c0761
Nick Sagona <nick@nolainteractive.com>
2020-09-15 14:39:24 -0500

    Update for pop-kettle 1.5.0

---
+++ kettle.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Kettle Include File
+ *
+ * Allows you to hook into the Kettle helper application by bootstrapping
+ * any additional runtime requirements, configurations or routes.
+ *
+ * The main Kettle $app object is accessible in this file.
+ */
+
